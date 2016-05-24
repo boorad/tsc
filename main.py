860d63faac3996f2dc8195ba96cc6e59d7906e04
Brad Anderson <brad@sankatygroup.com>
2016-05-24 15:58:59 -0400

    a bit of hardening, but still don't overcome formatting issues, so clear them before this works

--- main.py
+++ main.py
@@ -4,25 +4,50 @@ from openpyxl import load_workbook
 import sys, datetime
 
 CURR_STORE=""
+CURR_ROW=0
+
+def datetype(val):
+    return type(val) is datetime.datetime or type(val) is datetime.date
+
+def print_row(row):
+    global CURR_ROW
+
+    if CURR_ROW > 14:
+        return
+
+    CURR_ROW = CURR_ROW + 1
+    for cell in row:
+        value = cell.value
+#        if not isinstance(value, unicode):
+#            value = unicode(value, encoding='utf-8', errors='replace')
+        print value, type(value), repr(value)
+    print
 
 def process_row(row):
-    global CURR_STORE
+    global CURR_STORE, CURR_ROW
+
+    if CURR_ROW > 14:
+        #return
+        pass
 
     a = row[0].value
-    if a != CURR_STORE and type(a) is not datetime.datetime:
+
+    if a != CURR_STORE and not datetype(a):
         CURR_STORE = a
         return
 
-    if type(a) is datetime.datetime:
+    if datetype(a):
+        CURR_ROW = CURR_ROW + 1
         print CURR_STORE, ",", ",".join([str(cell.value) for cell in row])
 
 
 def main(fn="data/sample.xlsx"):
     wb = load_workbook(filename=fn, read_only=True)
-    ws = wb['Sheet1'] # ws is now an IterableWorksheet
+    ws = wb['Report'] # ws is now an IterableWorksheet
 
     for row in ws.rows:
         process_row(row)
+        #print_row(row)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
